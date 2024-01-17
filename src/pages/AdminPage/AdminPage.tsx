ec8623bff318c44b58c3b557887880f977a91093
Константин <turchakkonstantin@gmail.com>
2024-01-17 15:01:58 +0200

    pull from banatovskaya

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -2,7 +2,7 @@
 export const AdminPage = () => {
     return (
         <section className='admin-page'>
-            <h1 className="px-2 py-1 bg-orange-500">Панель администратора Electro-goods</h1>
+            <h1 className="px-2 py-1 bg-secondary">Панель администратора Electro-goods</h1>
         </section>
     );
 }
