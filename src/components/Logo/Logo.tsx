1a3b05aebd0ee3a224f5d9c615d224bb545e7c2c
Ira <banatovskaya@gmail.com>
2024-02-04 22:14:25 +0200

    add additionalContactPanel, make responsible header

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,13 @@
 export const Logo = () => {
     return (
-        <div className='logo hidden w-20 border-dotted border-secondary border-1 
-                        rounded-3xl justify-around h-7 pl-1
-                        xs:flex   
+        <div className='logo w-17 border-dotted border-secondary border-1 
+                        rounded-3xl justify-around h-7 pl-1 flex
+                        xs:h-7  
+                        
                         sm:w-40 
                         lg:pl-4 lg:w-64 lg:min-w-64 lg:h-12'>
-            <div className='md:w-1/2'><img className='logo-img w-full h-full' 
-                src="logo.jpg" alt="logo" />
+            <div className='md:w-1/2'>
+                <img className='logo-img w-full h-full' src="logo.jpg" alt="logo" />
             </div>
             <div className='logo-text w-1/3 text-[10px] leading-[10px]
                             hidden font-bold text-main self-center
