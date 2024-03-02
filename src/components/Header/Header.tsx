d3ac04fc88fc9ac5d291fa2bec5abf79abdaa4fe
Ira <banatovskaya@gmail.com>
2024-03-02 18:30:33 +0200

    layout catalogPanel

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,11 +11,27 @@ export const Header = () => {
                            xs:grid-cols-10 
                            lg:flex lg:flex-row lg:justify-between lg:items-center">
             <div className="col-span-2"><Logo /></div>
-            <div className='row-start-2 col-start-1 md:hidden'><CatalogPanel/></div>
-            <div className="row-start-2 col-start-4 col-span-8 md:col-start-1 md:col-span-12"><SearchPanel/></div>
-            <div className="row-start-1 col-start-5 col-span-4 xs:col-start-4"><ContactsPanel/></div>
-            <div className="row-start-1 col-start-7 col-span-1 xs:col-start-8 xs:col-span-1"><IconsPanel/></div>
-            <div className="row-start-1 col-start-12 col-span-1 justify-self-end xs:col-start-10 xs:col-span-1"><LanguagePanel/></div>
+            <div className='row-start-3 col-start-2 col-end-12
+                            xs:row-start-2 xs:col-start-1 xs:col-end-4
+                            md:hidden'>
+                <CatalogPanel/>
+            </div>
+            <div className="row-start-2 col-start-2 col-end-12 col-span-8 
+                            xs:col-start-5 xs:col-end-12 
+                            md:col-start-1 md:col-span-12">
+                <SearchPanel/>
+            </div>
+            <div className="row-start-1 col-start-5 col-span-4 xs:col-start-4">
+                <ContactsPanel/>
+            </div>
+            <div className="row-start-1 col-start-7 col-span-1 
+                            xs:col-start-8 xs:col-span-1">
+                <IconsPanel/>
+            </div>
+            <div className="row-start-1 col-start-12 col-span-1 justify-self-end 
+                            xs:col-start-10 xs:col-span-1">
+                <LanguagePanel/>
+            </div>
         </header>
     );
 }
