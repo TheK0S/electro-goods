0561dcdde1c704f800420a19b7170ffcde93242e
Ira <banatovskaya@gmail.com>
2024-03-06 21:29:31 +0200

    breadCrumb

--- src/components/breadCrumb/breadCrumb.tsx
+++ src/components/breadCrumb/breadCrumb.tsx
@@ -1,7 +1,17 @@
 export const BreadCrumb = () => {
-    return ( 
-        <div>
-            Осветительніе приборі \\Юлампочки\\галогенки
-        </div>
-    )
-}
+  return (
+    <nav>
+      <ol className="breadcrumb flex">
+        <li className="breadcrumb-item flex">
+          <a href="/">
+          <img className='logo-img inline-block h-5 w-5 mr-2' src="ELGOODS-64.png" alt="logo" />
+          ElectroGoods</a>
+        </li>
+        <li className="breadcrumb-item 
+                      before:content-['>'] before:mx-2 before:text-secondary before:font-bold"
+          >Розетки, вимикачі
+        </li>
+      </ol>
+    </nav>
+  );
+};
---
+++ src/components/filtr/filtrPanel.tsx
@@ -0,0 +1,4 @@
+export const FiltrPanel = () => {
+    return (
+        null
+    )}
